70b88e3ac0d7dda3766eb5004aaeeaebca53d05a
Mahak Gupta <guptamahak383@gmail.com>
2025-09-03 22:52:47 +0530

    AnagramCheck.java - Mahak Gupta

---
+++ Java-String/Level3/AnagramCheck.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class AnagramCheck {
+
+    public static boolean isAnagram(String s1, String s2) {
+        if (s1.length() != s2.length()) return false;
+
+        char[] a = s1.toCharArray();
+        char[] b = s2.toCharArray();
+
+        Arrays.sort(a);
+        Arrays.sort(b);
+
+        return Arrays.equals(a, b);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first string: ");
+        String s1 = sc.nextLine();
+        System.out.print("Enter second string: ");
+        String s2 = sc.nextLine();
+
+        System.out.println("Are Anagrams? " + isAnagram(s1, s2));
+        sc.close();
+    }
+}
