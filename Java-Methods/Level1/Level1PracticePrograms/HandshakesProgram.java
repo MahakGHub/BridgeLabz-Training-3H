96316b66fcb4ea5f30ad3c2cf3a97512791e61d6
Mahak Gupta <guptamahak383@gmail.com>
2025-09-03 21:58:56 +0530

    Added SimpleInterestProgram.java - Mahak Gupta

---
+++ Java-Methods/Level1/Level1PracticePrograms/HandshakesProgram.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class HandshakesProgram {
+    public static int calculateHandshakes(int n) {
+        return n * (n - 1) / 2;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of students: ");
+        int students = sc.nextInt();
+
+        int maxHandshakes = calculateHandshakes(students);
+        System.out.println("Maximum possible handshakes: " + maxHandshakes);
+    }
+}
+
