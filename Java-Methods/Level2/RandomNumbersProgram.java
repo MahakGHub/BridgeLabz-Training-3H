e4d3a00e0a8f71a1fcbebc2010e00a2db9154624
Mahak Gupta <guptamahak383@gmail.com>
2025-09-03 22:18:58 +0530

    Added RandomNumbersAnalysis.java - Mahak Gupta

---
+++ Java-Methods/Level2/RandomNumbersProgram.java
@@ -0,0 +1,29 @@
+public class RandomNumbersProgram {
+    public static int[] generate4DigitRandomArray(int size) {
+        int[] arr = new int[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = (int)(Math.random() * 9000) + 1000; // 4 digit
+        }
+        return arr;
+    }
+
+    public static double[] findAverageMinMax(int[] numbers) {
+        int min = numbers[0], max = numbers[0], sum = 0;
+        for (int n : numbers) {
+            sum += n;
+            min = Math.min(min, n);
+            max = Math.max(max, n);
+        }
+        double avg = sum / (double)numbers.length;
+        return new double[]{avg, min, max};
+    }
+
+    public static void main(String[] args) {
+        int[] arr = generate4DigitRandomArray(5);
+        System.out.print("Numbers: ");
+        for (int n : arr) System.out.print(n + " ");
+        System.out.println();
+        double[] stats = findAverageMinMax(arr);
+        System.out.println("Average: " + stats[0] + ", Min: " + stats[1] + ", Max: " + stats[2]);
+    }
+}
