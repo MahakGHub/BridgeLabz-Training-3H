84ea7b212f494f904232733e4e55f5481ced0fe6
Mahak Gupta <guptamahak383@gmail.com>
2025-09-04 16:45:03 +0530

    Mahak Gupta - Added CarRental.java

---
+++ Java_Const_Inst/Constructors/CarRental.java
@@ -0,0 +1,22 @@
+public class CarRental {
+    String customerName;
+    String carModel;
+    int rentalDays;
+    double costPerDay = 1000.0; // fixed rate
+
+    // Parameterized constructor
+    public CarRental(String customerName, String carModel, int rentalDays) {
+        this.customerName = customerName;
+        this.carModel = carModel;
+        this.rentalDays = rentalDays;
+    }
+
+    public double calculateTotalCost() {
+        return rentalDays * costPerDay;
+    }
+
+    public void display() {
+        System.out.println("Customer: " + customerName + ", Car: " + carModel +
+                ", Days: " + rentalDays + ", Total Cost: " + calculateTotalCost());
+    }
+}
